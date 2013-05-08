2bca1fabb3bcf8967a3d09c0b77823e721a9dac9
jae <xrrppy@gmail.com>
2013-05-08 10:01:22 -0400

    add

---
+++ cl/urls.py
@@ -0,0 +1,20 @@
+from django.conf.urls import patterns, include, url
+from django.views.generic.simple import direct_to_template
+
+# Uncomment the next two lines to enable the admin:
+from django.contrib import admin
+admin.autodiscover()
+
+urlpatterns = patterns('',
+    # Examples:
+    # url(r'^$', 'QMGR.views.home', name='home'),
+    # url(r'^QMGR/', include('QMGR.foo.urls')),
+
+    # Uncomment the admin/doc line below to enable admin documentation:
+    # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
+
+    # Uncomment the next line to enable the admin:
+    url(r'^admin/', include(admin.site.urls)),
+    url( r'^survey/', include('survey.urls')),
+
+)
